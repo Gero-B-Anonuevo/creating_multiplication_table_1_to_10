9355487105dca6bac1df88029737e88e71fb8825
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 21:31:46 +0800

    almost, needs to reduce outer loops

--- prog.py
+++ prog.py
@@ -4,7 +4,6 @@ while first_number < 11:
     for num in range(11):
         num = first_number
         for num2 in range(11):
-            num2 = first_number
-            print(num, end='')
+            print(num*num2, end='')
     print()
     first_number = first_number + 1
