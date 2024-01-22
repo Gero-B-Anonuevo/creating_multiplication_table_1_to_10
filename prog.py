0f57b64515edee01a57aa0413c2d30bbe119677d
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 21:32:56 +0800

    working, final

--- prog.py
+++ prog.py
@@ -1,7 +1,7 @@
 first_number = 1
 
 while first_number < 11:
-    for num in range(11):
+    for num in range(1):
         num = first_number
         for num2 in range(11):
             print(num*num2, end='')
