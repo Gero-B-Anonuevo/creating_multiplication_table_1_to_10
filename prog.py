ca6ac83a963d4ce10991fbc265b7ec5438426733
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 21:30:28 +0800

    big problem

--- prog.py
+++ prog.py
@@ -1,8 +1,10 @@
 first_number = 1
 
-while first_number < 10:
+while first_number < 11:
     for num in range(11):
         num = first_number
-        print(num, end='')
+        for num2 in range(11):
+            num2 = first_number
+            print(num, end='')
     print()
     first_number = first_number + 1
