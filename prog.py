94359cbdf185ace5a0000acaed01b630cf8dd8bd
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 21:36:01 +0800

    zeros removed

--- prog.py
+++ prog.py
@@ -3,7 +3,7 @@ first_number = 1
 while first_number < 11:
     for num in range(1):
         num = first_number
-        for num2 in range(11):
-            print(num*num2, end='')
+        for num2 in range(1, 11):
+            print(num*num2, end=' ')
     print()
     first_number = first_number + 1
